9bff993f75b3dc16adc885fe5d9ac47ee357ece7
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-10 23:32:50 +0530

    CSV without index and change columns name

---
+++ 31_problem.py
@@ -0,0 +1,7 @@
+# Write csv file without index and change column names
+import pandas as pd
+dic = {1:[1,2,3,4,5,6], 2:[9,8,7,6,5,4] , 3:[11,22,33,44,55,66]}
+S = pd.DataFrame(dic)
+print(S)
+S.to_csv("Test2.csv",index=False, header=["One","Two","Three"])
+print(S)
