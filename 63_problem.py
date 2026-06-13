47fab7641abb83c03270e54c25e9b3740ecba6cb
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-13 12:06:46 +0530

    USE-> dropna(thresh=4)

---
+++ 63_problem.py
@@ -0,0 +1,7 @@
+# Drop all Nan value Rows Using->> thresh=3
+# thresh stands for minimum number of non-NaN values required to keep a row (or column).
+import pandas as pd
+var = pd.read_csv("students.csv")
+print(var)
+var1 = var.dropna(thresh=3)
+print(var1)
