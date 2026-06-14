0f75707287ca0c5376c5a2a8eab8783abfac0635
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-14 13:50:10 +0530

    Fills Nan numeric values USE-> interpolate()

---
+++ 77_problem.py
@@ -0,0 +1,6 @@
+# Only interpolate() fills missing values in numeric columns.
+# It does not fill missing values in text
+import pandas as pd
+var = pd.read_csv("Test.csv")
+print(var)
+print(var.interpolate(method="linear"))
