6ffc7b78caed01de76b774efb402b6a35b4a91b5
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-14 13:51:24 +0530

    interpolate fnxns--> limit_direction,limit,method,axis

---
+++ 78_problem.py
@@ -0,0 +1,6 @@
+# Only interpolate() fills missing values in numeric columns.
+# It does not fill missing values in text
+import pandas as pd
+var = pd.read_csv("Test.csv")
+print(var)
+print(var.interpolate(limit_direction="both", limit=2 ,method="linear" ,axis=0))
