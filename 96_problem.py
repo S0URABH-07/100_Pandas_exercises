1278953d31f8e406e85313bfc817d1f048b638dc
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-15 21:58:53 +0530

    join two dataframes

---
+++ 96_problem.py
@@ -0,0 +1,5 @@
+# Join two dataframes 
+import pandas as pd
+var1 = pd.DataFrame({"A":[1,2,3,4,5],"B":[11,12,13,14,15]})
+var2 = pd.DataFrame({"C":[2,3,4,5,7],"D":[10,14,15,16,11]})
+print(var1.join(var2))
